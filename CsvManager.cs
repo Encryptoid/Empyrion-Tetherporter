37cc64a583f0dfd67baab6835bede133c48c905e
Encryptoid <penshhan@gmail.com>
2021-09-06 21:22:12 +0100

    Initial commit

---
+++ CsvManager.cs
@@ -0,0 +1,76 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Tetherporter
+{
+    interface IDatabaseManager
+    {
+        void SaveTetherportRecord(TetherporterRecord record);
+        TetherporterRecord LoadTetherporterRecord(string steamId);
+        bool LoadPortal(out PortalRecord portal);
+    }
+
+    class CsvManager: IDatabaseManager
+    {
+        private string _databasePath;
+        private Action<string> _log;
+        public CsvManager(string databasePath, Action<string> logFunc)
+        {
+            _databasePath = databasePath;
+            _log = logFunc;
+        }
+
+        public void SaveTetherportRecord(TetherporterRecord record)
+        {
+            var path = FormatFilePath(record.SteamId);
+
+            _log($"Logging record [[{record}]] to path {path}");
+
+            using(var stream = new StreamWriter(path))
+                using(var csv = new CsvWriter(stream, CultureInfo.InvariantCulture))
+                    csv.WriteRecords(new List<TetherporterRecord> { record });
+            
+        }
+
+        public bool LoadPortal(out PortalRecord portal)
+        {
+            portal = null;
+            var path = FormatPortalPath();
+
+            if (!File.Exists(path))
+                return false;
+
+            using (var stream = new StreamReader(path))
+            using (var csv = new CsvReader(stream, CultureInfo.InvariantCulture))
+                portal = csv.GetRecords<PortalRecord>().SingleOrDefault();
+
+            return portal != null;
+        }
+
+        public TetherporterRecord LoadTetherporterRecord(string steamId)
+        {
+            var path = FormatFilePath(steamId);
+
+            _log($"Loading record from: {path}");
+
+            using (var stream = new StreamReader(path))
+                using (var csv = new CsvReader(stream, CultureInfo.InvariantCulture))
+                    return csv.GetRecords<TetherporterRecord>().Single();                    
+        }
+
+        private string FormatFilePath(string steamId)
+        {
+            var fileName = $"{steamId}.tether";
+            return Path.Combine(_databasePath, fileName);
+        }
+
+        private string FormatPortalPath()
+        {
+            return Path.Combine(_databasePath, "portal.tether");
+        }
+    }
+}
---
+++ Tetherporter.cs
@@ -0,0 +1,67 @@
+﻿using Eleon;
+using Eleon.Modding;
+using EmpyrionModdingFramework;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Tetherporter
+{
+    public class Tetherporter : EmpyrionModdingFrameworkBase
+    {
+        private IDatabaseManager _dbManager;
+        protected override void Initialize()
+        {
+            ModName = "Tetherporter";
+            Log("TeleportTether is running from folder: " + Directory.GetCurrentDirectory());
+
+            var dbPath = "../Content/Mods/Tetherporter/Database";
+            var chatPrefix = "!";
+            CommandManager.CommandPrexix = chatPrefix;
+
+            _dbManager = new CsvManager(dbPath, ModAPI.Log);
+
+            CommandManager.CommandList.Add(new ChatCommand($"tetherport", (I) => Tetherport(I)));
+            CommandManager.CommandList.Add(new ChatCommand($"untether", (I) => Untether(I)));
+        }
+
+        private async Task Tetherport(MessageData messageData)
+        {
+            PlayerInfo player = (PlayerInfo)await RequestManager.SendGameRequest(
+                CmdId.Request_Player_Info, new Id() { id = messageData.SenderEntityId }
+                );
+
+            if(!_dbManager.LoadPortal(out var portal))
+            {
+                Log($"Entity {player.entityId}/{player.playerName} requested Tetherport but no portal was found.");
+                await MessagePlayer(player.entityId, $"No portal is currently active. Cannot Tetherport.", 10, MessagerPriority.Red);
+                return;
+            }
+
+            var record = new TetherporterRecord(player.steamId, player.entityId, player.playfield, player.pos, player.rot);
+
+            _dbManager.SaveTetherportRecord(record);
+
+            await TeleportPlayer(player.entityId, portal.Playfield, portal.PosX, portal.PosY, portal.PosZ, portal.RotX, portal.RotY, portal.RotZ);
+
+            Log($"Tetherported entity: {player.entityId}/{player.playerName} to portal.");
+        }
+
+        private async Task Untether(MessageData messageData)
+        {
+            PlayerInfo player = (PlayerInfo)await RequestManager.SendGameRequest(
+                CmdId.Request_Player_Info, new Id() { id = messageData.SenderEntityId }
+                );
+
+            if (!_dbManager.LoadPortal(out var portal))
+            {
+                Log($"Entity {player.entityId}/{player.playerName} requested Untether but no portal was found.");
+                await MessagePlayer(player.entityId, $"No event portal is active. Cannot Untether.", 10, MessagerPriority.Red);
+                return;
+            }
+
+            var record = _dbManager.LoadTetherporterRecord(player.steamId);
+
+            await TeleportPlayer(player.entityId, record.Playfield, record.PosX, record.PosY, record.PosZ, record.RotX, record.RotY, record.RotZ);
+        }
+    }
+}
