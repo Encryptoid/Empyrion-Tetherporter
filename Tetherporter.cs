67d6c813738e336735ba2ef96fe51dc2b68bee30
Encryptoid <penshhan@gmail.com>
2021-10-10 16:19:02 +0100

    Update to generic mod folder locator

--- Tetherporter.cs
+++ Tetherporter.cs
@@ -1,31 +1,28 @@
 ﻿using Eleon;
 using Eleon.Modding;
 using EmpyrionModdingFramework;
-using System;
-using System.Configuration;
+using InventoryManagement;
+using ModLocator;
 using System.IO;
 using System.Threading.Tasks;
+using EmpyrionModdingFramework.Database;
 
 namespace Tetherporter
 {
     public class Tetherporter : EmpyrionModdingFrameworkBase
     {
         private IDatabaseManager _dbManager;
+        private const string PortalFileName = "portal.tether";
         protected override void Initialize()
         {
             ModName = "Tetherporter";
 
-            var chatPrefix = "!";
-            CommandManager.CommandPrexix = chatPrefix;
+            var modLocator = new FolderLocator(Log);
+            _dbManager = new CsvManager(modLocator.GetDatabaseFolder(ModName));
 
-            var dbPath = GetDatabasePath();
-            if (string.IsNullOrEmpty(dbPath))
-                return;
-
-            _dbManager = new CsvManager(dbPath, ModAPI.Log);
-
-            CommandManager.CommandList.Add(new ChatCommand($"tetherport", (I) => Tetherport(I)));
-            CommandManager.CommandList.Add(new ChatCommand($"untether", (I) => Untether(I)));
+            CommandManager.CommandPrexix = "!";
+            CommandManager.CommandList.Add(new ChatCommand("tetherport", Tetherport));
+            CommandManager.CommandList.Add(new ChatCommand("untether", Untether));
         }
 
         private async Task Tetherport(MessageData messageData)
@@ -34,7 +31,7 @@ namespace Tetherporter
                 CmdId.Request_Player_Info, new Id() { id = messageData.SenderEntityId }
                 );
 
-            if(!_dbManager.LoadPortal(out var portal))
+            if(!_dbManager.LoadRecord<PortalRecord>(PortalFileName, out var portal))
             {
                 Log($"Entity {player.entityId}/{player.playerName} requested Tetherport but no portal was found.");
                 await MessagePlayer(player.entityId, $"No portal is currently active. Cannot Tetherport.", 10, MessagerPriority.Red);
@@ -43,70 +40,43 @@ namespace Tetherporter
 
             var record = new TetherporterRecord(player.steamId, player.entityId, player.playfield, player.pos, player.rot);
 
-            _dbManager.SaveTetherportRecord(record);
+            _dbManager.SaveRecord(FormatRecordId(player.steamId), record);
 
             await TeleportPlayer(player.entityId, portal.Playfield, portal.PosX, portal.PosY, portal.PosZ, portal.RotX, portal.RotY, portal.RotZ);
             Log($"Tetherported entity: {player.entityId}/{player.playerName} to portal.");
 
-            await MessagePlayer(player.entityId, $"Created teleport tether! Welcome to {portal.EventName}!", 10);
+            await MessagePlayer(player.entityId, $"Created Tetherporter tether! Welcome to {portal.EventName}!", 10);
         }
 
         private async Task Untether(MessageData messageData)
         {
             PlayerInfo player = (PlayerInfo)await RequestManager.SendGameRequest(
-                CmdId.Request_Player_Info, new Id() { id = messageData.SenderEntityId }
+                CmdId.Request_Player_Info, new Id { id = messageData.SenderEntityId }
                 );
 
-            if (!_dbManager.LoadPortal(out var portal))
+            if (!_dbManager.LoadRecord<PortalRecord>(PortalFileName, out _))
             {
                 Log($"Entity {player.entityId}/{player.playerName} requested Untether but no portal was found.");
                 await MessagePlayer(player.entityId, $"No event portal is active. Cannot Untether.", 10, MessagerPriority.Red);
                 return;
             }
 
-            if (!_dbManager.LoadTetherporterRecord(player.steamId, out var record))
+            if (!_dbManager.LoadRecord<TetherporterRecord>(FormatRecordId(player.steamId), out var tetherporterRecord))
             {
                 Log($"Entity {player.entityId}/{player.playerName} requester Untether but no tether was found.");
                 await MessagePlayer(player.entityId, $"No tether was found.", 5, MessagerPriority.Red);
                 return;
             }
 
-            await TeleportPlayer(player.entityId, record.Playfield, record.PosX, record.PosY, record.PosZ, record.RotX, record.RotY, record.RotZ);
+            await TeleportPlayer(player.entityId, tetherporterRecord.Playfield, 
+                tetherporterRecord.PosX, tetherporterRecord.PosY, tetherporterRecord.PosZ,
+                tetherporterRecord.RotX, tetherporterRecord.RotY, tetherporterRecord.RotZ);
         }
 
-
-        private string GetDatabasePath()
+        private string FormatRecordId(string steamId)
         {
-            /*
-             * Different hosting services run the Empyrion server executable from different locations
-             * In order to get around this, Tetherporter looks for a file: tetherporter.dbpath
-             * which should contain the path from the run directory to the Tetherport mod directory
-             * and be placed in the folder that the server executable is being ran from
-             * Examples I have come across:
-             * Running from: "/Empyrion - Dedicated Server/", File should contain: "Content/Mods/Tetherporter/Database"
-             * Running from: "/Empyrion - Dedicated Server/DedicatedServer", File should contain "../Content/Mods/Tetherporter/Database"
-            */
-
-            Log("TeleportTether is running from directory: " + Directory.GetCurrentDirectory());
-            var dbPathFileName = @"tetherporter.dbpath";
-            var dbPathFile = Path.Combine(Directory.GetCurrentDirectory(), dbPathFileName);
-
-            if (!File.Exists(dbPathFile)) //Check for tetherporter.dbpath
-            {
-                Log($"Tetherporter cannot locate it's database path. Please place a {dbPathFileName} file in the folder you are running the server from: {Directory.GetCurrentDirectory()}");
-                return null;
-            }
-
-            var dbPath = Path.GetFullPath(File.ReadAllText(dbPathFile).Trim()); //Trim spaces and parse relative paths
-
-            if (!Directory.Exists(dbPath)) //Check for Mods/Tetherporter/Database directory
-            {
-                Log($"Tetherporter cannot location it's database path, the full location it checked was: {dbPath}");
-                return null;
-            }
-
-            Log("Tetherporter will use the database path: " + dbPath);
-            return dbPath;
+            return $"{steamId}.tether";
         }
+
     }
 }
